ad9c198ee900b7c9930cdad131932cc5240ffdb1
cow chan <cowcfj@gmail.com>
2025-12-18 19:07:29 +0800

    refactor: 統一 HighlightManager.testable.js 導入使用源代碼
    將 serializeRange 導入從 Range.testable.js 改為源代碼 Range.js，
    與 COLORS 和 supportsHighlightAPI 的導入模式保持一致

--- tests/helpers/highlighter/core/HighlightManager.testable.js
+++ tests/helpers/highlighter/core/HighlightManager.testable.js
@@ -4,8 +4,8 @@
  * 此版本主要用於基礎邏輯測試
  */
 
-// 更新導入：使用源代碼替代已刪除的 testable 文件
-const { serializeRange } = require('../core/Range.testable.js');
+// 更新導入：使用源代碼替代 testable 文件
+const { serializeRange } = require('../../../../scripts/highlighter/core/Range.js');
 const { COLORS } = require('../../../../scripts/highlighter/utils/color.js');
 const { supportsHighlightAPI } = require('../../../../scripts/highlighter/utils/dom.js');
 
