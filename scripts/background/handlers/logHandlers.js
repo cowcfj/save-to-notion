7207beff96e1591c08a1c6d9d502380fec075104
cow chan <cowcfj@gmail.com>
2026-02-02 23:08:09 +0800

    feat(log): 增加日誌處理程序的註解與文檔說明

--- scripts/background/handlers/logHandlers.js
+++ scripts/background/handlers/logHandlers.js
@@ -1,6 +1,22 @@
+/**
+ * Log Handlers
+ *
+ * 處理與日誌相關的訊息請求，如導出除錯日誌。
+ *
+ * @module handlers/logHandlers
+ */
+
 import { LogExporter } from '../../utils/LogExporter.js';
 import { ErrorHandler, ErrorTypes } from '../../utils/ErrorHandler.js';
 
+/**
+ * 處理導出除錯日誌的請求
+ *
+ * @param {Object} message - 訊息物件，包含導出格式
+ * @param {Object} sender - 訊息發送者資訊
+ * @param {Function} sendResponse - 回應回調函數
+ * @returns {boolean} 返回 true 以支援並行非同步回應
+ */
 export const exportDebugLogs = (message, sender, sendResponse) => {
   try {
     // LogExporter.exportLogs 內含即時脫敏邏輯
@@ -20,6 +36,11 @@ export const exportDebugLogs = (message, sender, sendResponse) => {
   return true;
 };
 
+/**
+ * 創建日誌處理程序物件
+ *
+ * @returns {Object} 包含日誌處理函數的物件
+ */
 export function createLogHandlers() {
   return {
     exportDebugLogs,
