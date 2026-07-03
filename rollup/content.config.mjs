3c42b746b9637bbd184d7d65289534521848be28
cow chan <cowcfj@gmail.com>
2026-07-04 04:09:57 +0800

    refactor(content): switch content bundle to iife (#801)
    * refactor: switch content bundle to iife
    
    * test: simplify content script integration setup
    
    * test: align content script window cleanup
    
    * test: assert content script extraction result

--- rollup/content.config.mjs
+++ rollup/content.config.mjs
@@ -12,7 +12,7 @@ export default {
   input: 'scripts/content/index.js',
   output: {
     file: 'dist/content.bundle.js',
-    format: 'umd',
+    format: 'iife',
     name: 'ContentScript',
     sourcemap: isDev ? 'inline' : false,
     banner: '/* eslint-disable */\n/* Save to Notion - Content Script */',
